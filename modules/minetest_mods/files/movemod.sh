48a71d659e1a2a22647d0b2a27c576b1186bfc94
Martin Jahr <martin@desixma.org>
2021-02-23 13:38:29 +0100

    movemod

--- modules/minetest_mods/files/movemod.sh
+++ modules/minetest_mods/files/movemod.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
 # mode mods so that the unzipped directories match the server
-# usage from within ansible: movemod.sh <original-dir> <new-dir>
+# usage from within ansible: movemod.sh <zipfile> <mod-dir>
 # pre: zip file content exists and possibly the new-dir from a previous run
 # post: zip file content is moved to new-dir
 
-
-originaldir=$1
+zip=$1
 newdir=$2
 
-if [[ -z $originaldir || -z $newdir ]] ; then
-    echo "Usage: movemod.sh <original-dir> <new-dir>"
+if [[ -z $zip || -z $newdir ]] ; then
+    echo "Usage: movemod.sh <zip-file> <mod-dir>"
     exit 1
 fi
+
+mkdir -p /tmp/zip.$$
+mv $zip /tmp/zip.$$
+cd /tmp/zip.$$
+unzip $(basename $zip)
+
+if [[ $(ls -1 | wc -l) -ne 1 ]] ; then
+    echo "Unsupported zip file format"
+    exit 1
+fi
+
+unzipped=$(ls -1)
+
+if [[ ! -d $unzipped ]] ; then
+    echo "Unsupported zip file format"
+    exit 1
+fi
+
 
 if [[ -e $newdir ]] ; then
     rm -rf $newdir
 fi
 
-if [[ -e $originaldir ]] ; then
-    mv $originaldir $newdir
-fi
+mv $unzipped $newdir
+rm -rf /tmp/zip.$$
